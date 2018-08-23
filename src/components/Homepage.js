afafcdacf891635f7036c4b5272a34ba55c8a731
JDumadag25 <justin.dumadag@gmail.com>
2018-08-23 01:04:49 -0400

    begin adding money to account

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
 class Homepage extends React.Component {
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("hello");
+  }
+
   render(){
     return(
-      <input type='number'></input>
+      <div>
+      Add money to your Bank
+      <form onSubmit={this.handleSubmit}>
+        <input type='number'></input>
+      </form>
+      </div>
+
     )
   }
 }
