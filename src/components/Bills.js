af832eda3d38ad0f7466de44e5b3c37c405dad9b
JDumadag25 <justin.dumadag@gmail.com>
2019-02-28 17:09:52 -0500

    bills component

--- src/components/Bills.js
+++ src/components/Bills.js
@@ -3,9 +3,12 @@ import React from 'react'
 class Bills extends React.Component{
   render(){
     return(
-      <h1>Hello</h1>
+      <div>
+        <h5>Enter your bills for this month here:</h5>
+        <input type='number'></input>
+      </div>
     )
   }
 }
 
-export default Bills 
+export default Bills
