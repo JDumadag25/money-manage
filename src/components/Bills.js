c1e7e8f0890dbb389a635ddbc110f6b845cccdbe
JDumadag25 <justin.dumadag@gmail.com>
2019-03-01 22:50:21 -0500

    adding the bills to be subtracted from total

--- src/components/Bills.js
+++ src/components/Bills.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
 class Bills extends React.Component{
+
+  state = {
+    bills:[]
+  }
+
+  enterBills = () => {
+    console.log("click");
+  }
+
   render(){
     return(
       <div>
         <h5>Enter your bills for this month here:</h5>
         <input type='number'></input>
+        <button onClick={this.enterBills}></button>
       </div>
     )
   }
