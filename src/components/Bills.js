3a6afda910f485cfd7f90a687fdc90603cfd369d
JDumadag25 <justin.dumadag@gmail.com>
2019-04-23 22:30:25 -0400

    working on forms

--- src/components/Bills.js
+++ src/components/Bills.js
@@ -3,22 +3,40 @@ import React from 'react'
 class Bills extends React.Component{
 
   state = {
-    bills:[]
+    bills:{}
   }
 
   enterBills = () => {
     console.log("click");
   }
 
+  handleName = (e) => {
+    console.log(e.target.value);
+    this.setState({name:e.target.value})
+  }
+
+  handleInput = (e) => {
+    console.log(e.target.value);
+    this.setState({amount:e.target.value})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   render(){
     return(
       <div>
         <h5>Enter your bills for this month here:</h5>
-        <label>Name</label>
-        <input type='text'></input>
-        <label>Amount</label>
-        <input type='number'></input>
-        <button onClick={this.enterBills}>ENTER</button>
+        <form onSubmit={this.handleSubmit}>
+          <label>Name
+          <input type='text' onChange={this.handleName}></input>
+          </label>
+          <label>Amount
+          <input type='number' onChange={this.handleAmount}></input>
+          </label>
+          <button onClick={this.enterBills}>ENTER</button>
+        </form>
       </div>
 
     )
