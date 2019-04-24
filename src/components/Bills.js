1b464e6e94043ebd90b1cca19916ab282c4b7882
JDumadag25 <justin.dumadag@gmail.com>
2019-04-23 23:59:01 -0400

    multiple inputs

--- src/components/Bills.js
+++ src/components/Bills.js
@@ -3,25 +3,27 @@ import React from 'react'
 class Bills extends React.Component{
 
   state = {
-    bills:{}
+    bills:{},
+    name:'',
+    amount:''
   }
 
   enterBills = () => {
     console.log("click");
   }
 
-  handleName = (e) => {
-    console.log(e.target.value);
-    this.setState({name:e.target.value})
-  }
-
-  handleInput = (e) => {
-    console.log(e.target.value);
-    this.setState({amount:e.target.value})
+  handleChange = (e) => {
+  this.setState({
+    [e.target.name]: e.target.value
+   })
   }
 
   handleSubmit = (e) => {
     e.preventDefault()
+    let newname = this.state.name
+    let newamount = this.state.amount
+    let newbill = {newname : newamount}
+    console.log(newbill);
   }
 
   render(){
@@ -30,10 +32,10 @@ class Bills extends React.Component{
         <h5>Enter your bills for this month here:</h5>
         <form onSubmit={this.handleSubmit}>
           <label>Name
-          <input type='text' onChange={this.handleName}></input>
+          <input type='text' name='name' onChange={this.handleChange}></input>
           </label>
           <label>Amount
-          <input type='number' onChange={this.handleAmount}></input>
+          <input type='number' name='amount' onChange={this.handleChange}></input>
           </label>
           <button onClick={this.enterBills}>ENTER</button>
         </form>
